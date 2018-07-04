504b1c155fedfabe165e78f7865a5d492494e2e8
Andre Luiz da Silva <andreluiz1013@gmail.com>
2018-07-04 09:34:29 -0300

    first commit

---
+++ src/main/java/br/com/smart/vendas/SmartVendasApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.smart.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartVendasApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/smart/vendas/SmartVendasApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.smart.vendas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartVendasApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
